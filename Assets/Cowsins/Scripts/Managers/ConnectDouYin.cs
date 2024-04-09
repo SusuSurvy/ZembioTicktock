a7b2095138db066b98deb65b6d38c7a9d7fa475c
苏威 <suwei@pandadastudio.com>
2024-04-09 22:05:35 +0800

    抖音接入

---
+++ Assets/Cowsins/Scripts/Managers/ConnectDouYin.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using BestHTTP.WebSocket;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+using UnityEngine.Networking;
+
+public class ConnectDouYin: MonoBehaviour
+{
+    public Text DouyinText;
+    private WebSocket webSocket;
+    private string _url = "ws://127.0.0.1:9999";
+    public Action<Dictionary<string,string>> OnChatMessage;
+    public Action<Dictionary<string,string>> OnGiftMessage;
+    public Action<Dictionary<string, string>> OnEnterRoomMessage;
+    private void Start()
+    {
+        Debug.LogError("开启websocket");
+        webSocket = new WebSocket(new Uri(_url));
+        webSocket.OnOpen += OnOpen;
+        webSocket.OnMessage += OnMessageReceived;
+        webSocket.OnError += OnError;
+        webSocket.OnClosed += OnClosed;
+        Connect();
+    }
+    public void Connect()
+    {
+        try
+        {
+            webSocket.Open();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("链接失败");
+        }
+        Debug.LogError("发送连接");
+    }
+    void OnOpen(WebSocket ws)
+    {
+        DouyinText.text = "连接成功";
+        Debug.LogError("连接成功"); 
+    }
+    void OnMessageReceived(WebSocket ws, string msg)
+    {
+        var msgArray = JsonConvert.DeserializeObject<Dictionary<string,string>>(msg);
+        if (msgArray != null && msgArray.TryGetValue("type", out var type))
+        {
+            if (type == "ChatMessage")
+            {
+                OnChatMessage?.Invoke(msgArray);
+            }
+            if (type == "GiftMessage")
+            {
+                OnGiftMessage?.Invoke(msgArray);
+            }
+            if (type=="MemberMessage")
+            {
+                OnEnterRoomMessage?.Invoke(msgArray);
+            }
+        }
+    }
+    void OnClosed(WebSocket ws, UInt16 code, string message)
+    {
+        Debug.Log(message);
+        webSocket.Close();
+    }
+    private void OnDestroy()
+    {
+        if (webSocket != null && webSocket.IsOpen)
+        {
+            Debug.Log("连接断开");
+            webSocket.Close();
+        }}
+    void OnError(WebSocket ws, Exception ex)
+    {
+        DouyinText.text = "WebSocket出错:" + ex.Message;
+        Debug.Log("WebSocket出错:"+ex.Message);
+        webSocket.Close();
+    }
+    private bool _isDownLoad;
+    public IEnumerator DownLoadHeadImage(string url,Action<Texture2D> complete)
+    {
+        if (!_isDownLoad)
+        {
+            _isDownLoad = true;
+            var www = UnityWebRequestTexture.GetTexture(url);
+            yield return www.SendWebRequest();
+            var texture = DownloadHandlerTexture.GetContent(www);
+            complete?.Invoke(texture);
+            _isDownLoad = false;
+        }
+    }
+}
---
+++ Assets/Cowsins/Scripts/Managers/Entrance.cs
@@ -0,0 +1,76 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+public class Entrance : MonoBehaviour
+{
+  //  public Button button;
+   // public Configura configura;
+    private Microphone _microphone;
+    private ConnectDouYin _connectDouYin;
+    private AudioSource _audioSource;
+    private float _waitTime;
+
+    public Text Text;
+    public void Awake()
+    {
+      //  configura = JsonToos.ReadJson<Configura>("配置表");
+        _audioSource = GetComponent<AudioSource>();
+        //_microphone = GetComponent<Microphone>();
+        _connectDouYin = GetComponent<ConnectDouYin>();
+    }
+    
+    public void Start()
+    {
+      //  _audioSource.volume = configura.音量;
+      //  button.onClick.AddListener(() => button.gameObject.SetActive(false));
+        InitDouYinConnect();
+    }
+    
+    public void Update()
+    {
+      //  if (button.gameObject.activeSelf) return;
+        if (_waitTime > 0) _waitTime -= Time.deltaTime;
+        else
+        {
+        //    _waitTime = configura.重置时间;
+        }
+    }
+    private void InitDouYinConnect()
+    {
+        if (!_connectDouYin.enabled) return;
+        _connectDouYin.OnGiftMessage = data =>
+        {
+            var gift = $"感谢{data["name"]}";
+            var url = data["head_img"];
+            StartCoroutine(_connectDouYin.DownLoadHeadImage(url, teu =>
+            {
+                //TODO
+            }));
+        };
+        _connectDouYin.OnEnterRoomMessage = data =>
+        {
+            var url = data["head_img"];
+            var name = $"{data["name"]}进入房间，生成丧尸";
+            Text.text = name;
+            EnemyManager.Instance.CreateEnemy();
+            StartCoroutine(_connectDouYin.DownLoadHeadImage(url, teu =>
+            {
+                Text.text = name;
+                //TODO
+            }));
+        };
+        _connectDouYin.OnChatMessage = data =>
+        {
+            var content = data["content"];
+            var name = data["name"];
+            if (name.Contains("用户")) return;
+            var url = data["head_img"];
+           // if (!ReGexTools.IsChinese(content)) return;
+            StartCoroutine(_connectDouYin.DownLoadHeadImage(url, teu =>
+            {
+                //TODO
+            }));
+        };
+    }
+}
